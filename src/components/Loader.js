5206c4e8a5635bdd8a8e680d1c0ff91dbe4d93d7
BLAINVILLE <logisthit@gmail.com>
2022-10-22 20:20:18 +0200

    add loader animation

---
+++ src/components/Loader.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import LetterK from '../images/LetterK.svg'
+import HouseSvg from '../images/HouseSvg.svg'
+import LetterS from '../images/LetterS.svg'
+import LetterA from '../images/LetterA.svg'
+import '../styles/animation.css'
+
+const Loader = () => {
+    return (
+        <div class="loader">
+            <img src={LetterK} alt='letter logo' className="letter" />
+            <img src={HouseSvg} alt='letter logo' className="letter" />
+            <img src={LetterS} alt='letter logo' className="letter" />
+            <img src={LetterA} alt='letter logo' className="letter" />
+        </div>
+    );
+};
+
+export default Loader;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Loader from '../components/Loader'
 import Banner from '../components/Banner'
 import ListCard from '../components/ListCard'
+
 
 // ==============================================================
 
 function Home() {
     return (
         <div>
+            <Loader />
             <Banner />
             <ListCard />
         </div>
