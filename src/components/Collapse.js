8a2d9d0edfb38b474d36c066f289fccb96b027fc
BLAINVILLE <logisthit@gmail.com>
2022-10-26 18:49:40 +0200

    amélioration composant Collapse

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -12,6 +12,9 @@ function Collapse(props) {
 
     const content = useRef(null)
 
+    const { size } = props
+    const className = size === 'large' ? 'about__page' : 'logement-page'
+
     const togglecollapse = () => {
         setActiveState(setActive === "" ? "active" : "")
         setHeightState(
@@ -23,7 +26,7 @@ function Collapse(props) {
     }
 
     return (
-        <div className="logement-page">
+        <div className={className}>
             <button className={`collapse ${setActive}`} onClick={togglecollapse}>
                 <p className="collapse-title">{props.title}</p>
                 <img src={Chevron} alt="chevron" className={`${setRotate}`} />
@@ -40,37 +43,3 @@ function Collapse(props) {
 }
 
 export default Collapse
-
-export function CollapseLarge(props) {
-    const [setActive, setActiveState] = useState("")
-    const [setHeight, setHeightState] = useState("0px")
-    const [setRotate, setRotateState] = useState("collapse-icon")
-
-    const content = useRef(null)
-
-    const togglecollapse = () => {
-        setActiveState(setActive === "" ? "active" : "")
-        setHeightState(
-            setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
-        );
-        setRotateState(
-            setActive === "active" ? "collapse-icon" : "collapse-icon rotate"
-        );
-    }
-
-    return (
-        <div className="about__page">
-            <button className={`collapse ${setActive}`} onClick={togglecollapse}>
-                <p className="collapse-title">{props.title}</p>
-                <img src={Chevron} alt="chevron" className={`${setRotate}`} />
-            </button>
-            <div
-                ref={content}
-                style={{ maxHeight: `${setHeight}` }}
-                className="collapse-content"
-            >
-                <div><div className="collapse-text">{props.content}</div></div>
-            </div>
-        </div>
-    )
-}
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CollapseLarge } from "../components/Collapse"
+import Collapse from "../components/Collapse"
 import BackgroundAbout from '../images/D_About.webp'
 import '../styles/About.css'
 
@@ -11,21 +11,25 @@ function About() {
       <img src={BackgroundAbout} alt='bannière paysage' className='banner-img' />
       <main>
         <div className='container-about'>
-          <CollapseLarge
+          <Collapse
             title="Fiabilité"
             content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+            size='large'
           />
-          <CollapseLarge
+          <Collapse
             title="Respect"
             content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+            size='large'
           />
-          <CollapseLarge
+          <Collapse
             title="Service"
             content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+            size='large'
           />
-          <CollapseLarge
+          <Collapse
             title="Responsabilité"
             content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+            size='large'
           />
         </div>
       </main>
