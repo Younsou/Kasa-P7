4db0832752ec21dcd0b87f7a95d25e8787b6a245
BLAINVILLE <logisthit@gmail.com>
2022-11-03 17:57:28 +0100

    correction margin & add position relative to banner

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -8,7 +8,7 @@ function Error() {
     return (
         <div className='container_error'>
             <p className='error_number'>404</p>
-            <h1 className='error_text'>Oups! La page que vous demandez n'existe pas.</h1>
+            <h1 className='error_text'>Oups! La page que vous&nbsp;demandez&nbsp;n'existe pas.</h1>
             <Link to="/">
                 <p className='text_return'>Retourner sur la page d’accueil</p>
             </Link>
